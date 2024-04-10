8201f57bf8d966f7f54c8d86a0e053469643cc41
lari2806 <124395564+lari2806@users.noreply.github.com>
2024-04-10 16:20:26 -0300

    Update Teste.java

--- POO_PESSOAS/Teste.java
+++ POO_PESSOAS/Teste.java
@@ -3,19 +3,17 @@ public class Teste{
     public static void main(String[]args){
 
     Pessoa Cousin = new Pessoa("Larissa", 2007);
-    System.out.println(Cousin.Saudação() + "\n");
+    System.out.println(Cousin.Saudação());
     
     Aluno Student = new Aluno("César", 2008, "Informática", 06234);  
-    System.out.println(Student.SaudaçãoStud());
+    System.out.println(Student.Saudação());
 
     Professor Teacher = new Professor("Alexandra", 1996, "Informática", 98029, 3);
     System.out.println(Teacher.Saudação());
-    System.out.println(Teacher.SaudaçãoStud());
-    System.out.println(Teacher.SaudaçãoTeacher() + "\n");
+
     
-    Funcionário Employees = new Funcionário("Lucy", 2000, 5000 , "Doutora");
-    System.out.println(Employees.SaudaçãoFunc() + "\n");
-    System.out.println(Student.Saudação() + "\n");
+    Funcionário Employees = new Funcionário("Lucy", 2000, 5000 , "Doutora", 2222);
+    System.out.println(Employees.Saudação() + "\n");;
     
 
     }
