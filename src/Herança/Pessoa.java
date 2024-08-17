82e359e2050c701c83d26a7cc96813c7eabbea48
Larissa <larissa.rfs28@gmai.com>
2024-08-17 17:08:13 -0300

    Modificação no arquivo.

--- src/Herança/Pessoa.java
+++ src/Herança/Pessoa.java
@@ -6,30 +6,30 @@ import java.time.format.DateTimeFormatter;
 
 
 public class Pessoa{
-    protected String Nome;
-    protected int Nascimento;
-    public String Apelido = "";
+    protected String nome;
+    protected int nascimento;
+    public String apelido = "";
 
-    public Pessoa(String Nome, int Nascimento){
-        this.Nome = Nome;
-        this.Nascimento = Nascimento;
+    public Pessoa(String nome, int nascimento){
+        this.nome = nome;
+        this.nascimento = nascimento;
 
     }
     protected int calcIdade(){
-        return(LocalDate.now().getYear()-this.Nascimento);
+        return(LocalDate.now().getYear()-this.nascimento);
     }
     public String Saudação(){
         LocalTime hora_atual = LocalTime.now();
         String horaformat = hora_atual.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
         int hora = hora_atual.getHour();
         if (hora >= 5 && hora <= 11){
-            return horaformat+ " Bom Dia,meu nome é " +this.Nome + " e tenho " + calcIdade() + " anos ";
+            return horaformat+ " Bom Dia,meu nome é " +this.nome + " e tenho " + calcIdade() + " anos ";
         } else if(hora >=12 && hora <=18){
-            return horaformat+" Boa Tarde,meu nome é  " +this.Nome +  " e tenho " + calcIdade() + " anos ";
+            return horaformat+" Boa Tarde,meu nome é  " +this.nome +  " e tenho " + calcIdade() + " anos ";
         }else if(hora >=19 && hora <=23){
-            return horaformat+" Boa Noite, meu nome é  " +this.Nome + " e tenho " + calcIdade() + " anos ";
+            return horaformat+" Boa Noite, meu nome é  " +this.nome + " e tenho " + calcIdade() + " anos ";
         }else if(hora >=0 && hora <=4){
-            return horaformat+" Vai Dormir, me chamo " +this.Nome + " e tenho " +calcIdade() + " anos ";
+            return horaformat+" Vai Dormir, me chamo " +this.nome + " e tenho " +calcIdade() + " anos ";
         }else{
             return"erro na hora";
         }
