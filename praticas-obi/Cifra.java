97745bf157ad406cdde486d60478aa77611e898e
Larissa <larissa.rfs28@gmail.com>
2025-06-10 20:52:55 -0300

    Formatando linhas de código (organização)

--- praticas-obi/Cifra.java
+++ praticas-obi/Cifra.java
@@ -14,10 +14,6 @@ public class Cifra {
         String[] vogais = {alfabeto[0], alfabeto[4], alfabeto[8], alfabeto[14], alfabeto[20]};
 
         String[] consoantes = {alfabeto[1], alfabeto[2], alfabeto[3], alfabeto[5], alfabeto[6], alfabeto[7], alfabeto[9], alfabeto[10], alfabeto[11], alfabeto[12], alfabeto[13], alfabeto[15], alfabeto[16], alfabeto[17], alfabeto[18], alfabeto[19], alfabeto[21], alfabeto[22], alfabeto[23], alfabeto[24], alfabeto[25]};
-
-      
-
-            
         
         for (int i = 0; i < palavraNaoCifrada.length; i++) {
             for (int j = 0; j < vogais.length; j++) {
@@ -30,16 +26,14 @@ public class Cifra {
                     } 
                     if(palavraNaoCifrada[i].equals(consoantes[j2])){
 
-                        
-                    String consoanteCifrada = (consoantes[ j2 + 1]) ;             
-
-                    System.out.println(consoantes[j2] + vogais[j - i]  + consoanteCifrada);
-                    
-                    
-
+                    String consoanteCifrada = (consoantes[ j2 + 1]) ;    
+                           
+                    System.out.println(consoantes[j2] + vogais[j-i] + consoanteCifrada);
+                    }    
                 }
             }
         }
-        scanner.close();
-    }
-}}
+    scanner.close();
+   }
+}
+
